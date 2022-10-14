3ae4fe51ef13659e8bcc622554357eb81324dd14
whawo <whawo@naver.com>
2022-10-14 17:31:48 +0900

    프로젝트생성

---
+++ fourweeks/src/main/java/com/kh/fourweeks/FourweeksApplication.java
@@ -0,0 +1,13 @@
+package com.kh.fourweeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourweeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourweeksApplication.class, args);
+	}
+
+}
---
+++ fourweeks/src/test/java/com/kh/fourweeks/FourweeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.fourweeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourweeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
