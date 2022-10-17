b006e5c551f7dcebca20754df97cf4d1fa3f3608
jeeweon <jeeweon@hanmail.net>
2022-10-17 12:49:54 +0900

    jeeweon 브랜치 테스트

--- fourweeks/src/main/java/com/kh/fourweeks/testcontroller/TestController.java
+++ fourweeks/src/main/java/com/kh/fourweeks/testcontroller/TestController.java
@@ -2,7 +2,9 @@ package com.kh.fourweeks.testcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class TestController {
 @Autowired
 private JdbcTemplate jdbcTemplate;
