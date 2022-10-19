5c191e5f04267ced537e1d27e4dac42164e420be
soo231 <a995115@gmail.com>
2022-10-19 21:07:23 +0900

    - 홈컨트롤러에 홈주소 매핑 생성 - 헤더/푸터 생성(아이콘,이미지없음) - commons1.css, reset.css 업로드 - home.jsp 생성

--- fourweeks/src/main/java/com/kh/fourweeks/controller/HomeController.java
+++ fourweeks/src/main/java/com/kh/fourweeks/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.kh.fourweeks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	
+	@RequestMapping("/")
+	public String home(Model model) {
+		return "home";
+	}
 
 }
