95b1c5426291c98c6866d0ee32cebf235c1d517a
jeeweon <jeeweon@hanmail.net>
2022-10-20 22:19:48 +0900

    내가 인증글 쓸 수 있는 챌린지로 이름 변경

--- fourweeks/src/main/java/com/kh/fourweeks/vo/ConfirmAbleChalListVO.java
+++ fourweeks/src/main/java/com/kh/fourweeks/vo/ConfirmAbleChalListVO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
-public class MyIngChalListVO {
+public class ConfirmAbleChalListVO {
 	private String userId;
 	private int chalNo;
 	private String chalTitle;
