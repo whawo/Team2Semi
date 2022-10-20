de8f91d445f019b840b1314c1efd5c85e5984a7c
jeeweon <jeeweon@hanmail.net>
2022-10-20 17:42:25 +0900

    챌린지 인증 글쓰기 구현중

--- fourweeks/src/main/java/com/kh/fourweeks/controller/ChalController.java
+++ fourweeks/src/main/java/com/kh/fourweeks/controller/ChalController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +18,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.fourweeks.constant.SessionConstant;
+import com.kh.fourweeks.entity.ChalConfirmDto;
 import com.kh.fourweeks.entity.ChalDto;
 import com.kh.fourweeks.entity.ParticipantDto;
+import com.kh.fourweeks.repository.ChalConfirmDao;
 import com.kh.fourweeks.repository.ChalDao;
 import com.kh.fourweeks.service.ChalService;
 
@@ -29,6 +30,9 @@ import com.kh.fourweeks.service.ChalService;
 public class ChalController {
 	@Autowired
 	private ChalDao chalDao;
+	
+	@Autowired
+	private ChalConfirmDao confirmDao;
 	
 	@Autowired
 	private ChalService chalService;
@@ -71,10 +75,26 @@ public class ChalController {
 		return "redirect:detail";
 	}
 	
-	@GetMapping("/detail")//상세 조회(단일)
-	public String detail(@ModelAttribute ChalDto chalDto,
-			Model model) {
-		model.addAttribute("chalDto", chalDao.selectOne(chalDto.getChalNo()));
-		return "chal/detail";
+	@GetMapping("/confirm")
+	public String confirm() {
+		return "chal/confirm";
 	}
+	
+	@PostMapping("/confirm")
+	public String confirm(@ModelAttribute ChalConfirmDto confirmDto,
+			@ModelAttribute ChalDto chalDto,
+			RedirectAttributes attr,
+			HttpSession session) {
+		String memberId = (String)session.getAttribute(SessionConstant.ID);
+		confirmDto.setUserId(memberId);
+		
+		int confirmNo = confirmDao.sequence();
+		confirmDto.setConfirmNo(confirmNo);
+		
+		confirmDao.write(confirmDto);
+		
+		attr.addAttribute("confirmNo", confirmNo);
+		return "redirect:/confirm/detail";
+	}
+	
 }
