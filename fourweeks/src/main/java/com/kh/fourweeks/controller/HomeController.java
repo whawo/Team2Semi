d9f8eb52411293158eb63ae7c96f9579d1d65716
soo231 <a995115@gmail.com>
2022-10-20 14:44:34 +0900

    - 헤더/푸터 아이콘 추가 - commons1 일부 수정

--- fourweeks/src/main/java/com/kh/fourweeks/controller/HomeController.java
+++ fourweeks/src/main/java/com/kh/fourweeks/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.kh.fourweeks.controller;
 
 import org.springframework.stereotype.Controller;
-
-import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String home(Model model) {
-		return "home";
+	public String home() {
+	return "home";
 	}
 }
