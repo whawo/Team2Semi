072eabdefa40994078116b3fba26c11364e8e7ff
jeeweon <jeeweon@hanmail.net>
2022-10-21 02:42:43 +0900

    인증글 작성 가능한 챌린지 조회 구문 변경
    - 유저 아이디 조건 추가

--- fourweeks/src/main/java/com/kh/fourweeks/repository/ChalConfirmDao.java
+++ fourweeks/src/main/java/com/kh/fourweeks/repository/ChalConfirmDao.java
@@ -1,10 +1,14 @@
 package com.kh.fourweeks.repository;
 
+import java.util.List;
+
 import com.kh.fourweeks.entity.ChalConfirmDto;
+import com.kh.fourweeks.vo.ConfirmAbleChalListVO;
 
 public interface ChalConfirmDao {
 	int sequence();
 	void write(ChalConfirmDto confirmDto);
 	boolean update(ChalConfirmDto confirmDto);
-	
+
+	List<ConfirmAbleChalListVO> selectList(String userId);
 }
