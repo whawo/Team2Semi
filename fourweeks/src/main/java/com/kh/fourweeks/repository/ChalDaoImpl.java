d127a2a1d530e10b17b81f17af1b6179786ba116
jeeweon <jeeweon@hanmail.net>
2022-10-21 14:37:57 +0900

    chalAttachment 메소드 이름 변경

--- fourweeks/src/main/java/com/kh/fourweeks/repository/ChalDaoImpl.java
+++ fourweeks/src/main/java/com/kh/fourweeks/repository/ChalDaoImpl.java
@@ -52,7 +52,7 @@ public class ChalDaoImpl implements ChalDao {
 	}
 
 	@Override
-	public void connectAttachment(int chalNo, int attachmentNo) {
+	public void chalAttachment(int chalNo, int attachmentNo) {
 		String sql = "insert into chal_img(chal_no, attachment_no) values(?, ?)";
 		Object[] param = {chalNo, attachmentNo};
 		
