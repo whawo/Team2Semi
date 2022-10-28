04535012aeace22d8cf6cca924b65a1e0d899b13
jeeweon <jeeweon@hanmail.net>
2022-10-28 14:41:40 +0900

    오늘 시작/종료 챌린지 수 통계용 VO

--- fourweeks/src/main/java/com/kh/fourweeks/vo/StartEndTodayVO.java
+++ fourweeks/src/main/java/com/kh/fourweeks/vo/StartEndTodayVO.java
@@ -1,4 +1,13 @@
 package com.kh.fourweeks.vo;
 
-public class ChalReportVO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//오늘 시작/종료하는 챌린지 수 통계용
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class StartEndTodayVO {
+	private int scnt;
+	private int ecnt;
 }
