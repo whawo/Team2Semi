eaf5df73d7c119154b16d81a88931049ba99256e
jeeweon <jeeweon@hanmail.net>
2022-11-01 10:31:57 +0900

    챌린지 상세 타이머 구현을 위한 비동기 처리용 메소드 생성

--- fourweeks/src/main/java/com/kh/fourweeks/restcontroller/ChalRestController.java
+++ fourweeks/src/main/java/com/kh/fourweeks/restcontroller/ChalRestController.java
@@ -12,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.fourweeks.constant.SessionConstant;
+import com.kh.fourweeks.entity.ChalDto;
 import com.kh.fourweeks.entity.ChalMyDetailDto;
 import com.kh.fourweeks.repository.ChalConfirmDao;
+import com.kh.fourweeks.repository.ChalDao;
+import com.kh.fourweeks.vo.ChalDetailVO;
 import com.kh.fourweeks.vo.ConfirmDaysVO;
 
 @CrossOrigin(origins = {"http://127.0.0.1:5500"})
@@ -23,8 +26,22 @@ public class ChalRestController {
 	@Autowired
 	private ChalConfirmDao confirmDao;
 	
+	
+	@Autowired
+	private ChalDao chalDao;
+	
 	@GetMapping("/confirm_days")
 	public List<ConfirmDaysVO> myConfirmDays(@ModelAttribute ChalMyDetailDto chalMyDetailDto, HttpSession session) {
 		return confirmDao.myConfirmDays(chalMyDetailDto.getChalNo(), (String)session.getAttribute(SessionConstant.ID));
+	}
+	
+	@GetMapping("/chal_end_date")
+	public ChalDetailVO chalEndDate (int chalNo) {
+		return chalDao.selectEndDday(chalNo);
+	}
+	
+	@GetMapping("/chal_detail")
+	public ChalDto chalDetail (int chalNo) {
+		return chalDao.selectOne(chalNo);
 	}
 }
